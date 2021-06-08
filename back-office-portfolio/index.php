d53b6bff7edd76bf374bfb61b2c24e2ad851563e
nathbout2 <nathbout2@gmail.com>
2021-06-08 14:30:49 +0200

    button-effect

--- back-office-portfolio/index.php
+++ back-office-portfolio/index.php
@@ -5,10 +5,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
+   <link rel="stylesheet" href="style.css"> 
 </head>
 <body>
     
-<button><a href="register-form.php">S'inscrire</a></button>
-<button><a href="login-form.php">Se connecter</a></button>
+
+<a href="register-form.php" class="btn"><span>S'inscrire</span></a>
+<a href="login-form.php" class="btn"><span>Se connecter</span></a>
+
+
+<script>
+
+const btn = document.getElementsByClassName('btn');
+
+    btn[0].onmousemove = function(e){
+        const x = e.pageX - btn[0].offsetLeft;
+        const y = e.pageY - btn[0].offsetTop;
+        btn[0].style.setProperty('--x', x +'px');
+        btn[0].style.setProperty('--y', y +'px');
+    }
+
+    btn[1].onmousemove = function(e){
+        const x = e.pageX - btn[1].offsetLeft;
+        const y = e.pageY - btn[1].offsetTop;
+
+        btn[1].style.setProperty('--x', x +'px');
+        btn[1].style.setProperty('--y', y +'px');
+    }
+
+
+</script>
+
 </body>
 </html>
+
--- back-office-portfolio/main.js
+++ back-office-portfolio/main.js
@@ -12,3 +12,6 @@ document.getElementById('submit').addEventListener('click',  (event)=>{
         }
 });
 
+
+
+
